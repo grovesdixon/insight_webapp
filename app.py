9b84e1e39279185e32a05b586d9e133652c01911
Groves Dixon <grovesdixon@Grovess-MacBook-Pro.local>
2020-06-16 07:07:07 -0500

    update with change from piechart to barplot

--- app.py
+++ app.py
@@ -14,6 +14,87 @@ image = Image.open('data/logo.png')
 st.image(image, format='PNG')
 
 
+#load the model and example data and the model
+X_example = pd.read_csv('data/readmission_example_patients.csv', index_col=0)
+loaded_model = joblib.load(open('data/rf_model.joblib', 'rb'))
+
+#get the example patient names
+patient_options = X_example.index.tolist()
+
+#get patient selection from user
+patient = st.sidebar.selectbox('Select patient:', patient_options)
+psub = X_example.loc[patient,:] #load data for prediction
+
+
+#activate
+#predict probability from model
+p_prob = loaded_model.predict_proba([psub])[0,1]
+p_prob = np.round(p_prob, 3)
+readmission_size = p_prob*100
+stay_home_size = 100.0 - readmission_size
+
+
+
+# #plot probability
+pcts = [readmission_size, stay_home_size]
+names = ['readmission', 'safe']
+df = pd.DataFrame({'x':patient, 'percentage':pcts, 'prediction':names})
+fig = px.bar(df, x="percentage", y="x", color='prediction', orientation='h',
+             height=300,
+             hover_data=["prediction", "percentage"],
+             color_discrete_map={'readmission':'firebrick',
+                                  'safe':'darkgrey'})
+fig.update_layout(
+    title='Probability readmission within 30 days of discharge',
+    yaxis_title="",
+    plot_bgcolor='rgba(0,0,0,0)')
+st.plotly_chart(fig, use_container_width=True)
+
+
+
+#------- PLOT SUGGESTION -------#
+
+#images
+image_paths = {
+'very_high': 'data/stop.png',
+'high': 'data/stop.png',
+'medium': 'data/caution.png',
+'low': 'data/ok.png'
+}
+
+#cpations
+captions = {
+'very_high':
+'This patient is at very high risk of readmission within 30 days. Dischage strongly discouraged.',
+'high':
+'This patient is at high risk of readmission within 30 days. Dischage discouraged.',
+'medium':
+'This patient posses some risk for readmission within 30 days. Dischage with caution.',
+'low':
+'This patient is of low concern for readmission.'}
+
+#function to plot
+def plot_recommendation_image(image_path, caption, image_width):
+	rec_image = Image.open(image_path)
+	st.image(rec_image, width = image_width, format='PNG')
+	st.write(report)
+	st.write(caption)
+
+report = 'The probability that the {} will be readmitted within 30 days is {}%'.format(patient,p_prob*100)
+image_width = 80
+if p_prob > 0.75:
+	plot_recommendation_image(image_paths['very_high'], captions['very_high'], image_width)
+elif p_prob > 0.49:
+	plot_recommendation_image(image_paths['high'], captions['high'], image_width)
+elif p_prob > 0.1:
+	plot_recommendation_image(image_paths['medium'], captions['medium'], image_width)
+else:
+	plot_recommendation_image(image_paths['low'], captions['low'], image_width)
+	pass
+
+
+#------- SHOW PATIENT DATA -------#
+
 #format the table to look prettier
 X_view = pd.read_csv('data/readmission_example_patients_view.csv', index_col=0)
 col_change = {'INSURANCE':'insurance type',
@@ -38,64 +119,8 @@ num_cols = ['admission duration',
 'total drugs prescribed']
 X_view[num_cols] = X_view[num_cols].astype('int')
 
-
-#load the model and example data and teh model
-X_example = pd.read_csv('data/readmission_example_patients.csv', index_col=0)
-loaded_model = joblib.load(open('data/rf_model.joblib', 'rb'))
-
-#get the example patient names
-patient_options = X_example.index.tolist()
-
-
-#get patient selection from user
-patient = st.sidebar.selectbox('Select patient:', patient_options)
-psub = X_example.loc[patient,:] #load data for prediction
+#subset for patient
 psub_view = X_view.loc[patient,:] #load data for viewing
-
-
-#predict probability from model
-p_prob = loaded_model.predict_proba([psub])[0,1]
-p_prob = np.round(p_prob, 3)
-readmission_size = p_prob*100
-stay_home_size = 100.0 - readmission_size
-
-
-
-# #plot probability
-pcts = [stay_home_size, readmission_size]
-names = ['safe', 'readmission risk']
-df = pd.DataFrame({'pcts':pcts, 'names':names})
-fig = px.pie(df, values='pcts', names='names',
-             color='names',
-             color_discrete_map={'readmission risk':'firebrick',
-                                 'safe':'darkgrey'})
-fig.update_layout(showlegend=True)
-st.plotly_chart(fig, use_container_width=True)
-
-
-#plot recommendation symbol
-report = 'The probability that the {} will be readmitted within 30 days is {}'.format(patient,p_prob)
-if p_prob > 0.75:
-	rec_image = Image.open('data/stop.png')
-	st.image(rec_image, format='PNG')
-	st.write(report)
-	st.write('This patient is at very high risk of readmission within 30 days. Confirmation with physician required')
-elif p_prob > 0.49:
-	rec_image = Image.open('data/stop.png')
-	st.image(rec_image, format='PNG')
-	st.write(report)
-	st.write('This patient is at high risk of readmission within 30 days. Suggest confirmation with physician')
-elif p_prob > 0.1:
-	rec_image = Image.open('data/caution.png')
-	st.image(rec_image, format='PNG')
-	st.write(report)
-	st.write('This patient posses some risk for readmission within 30 days and should be discharged with caution.')
-else:
-	rec_image = Image.open('data/ok.png')
-	st.image(rec_image, format='PNG')
-	st.write(report)
-	st.write('This patient is of low concern for readmission.')
-	pass
 
 #print the patient's data to streamlit screen
 '''
